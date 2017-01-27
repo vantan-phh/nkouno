442179b995c40608b415f58b355a8c299e7b8095
高槻やよい <qazwsxedcrfvtgbyhnujmikol@takatsukiyayoi.local>
2017-01-27 16:04:41 +0900

    死にたい

--- public/javascripts/graph.js
+++ public/javascripts/graph.js
@@ -5,41 +5,41 @@ function createGraph(multiData, state) {
   userData = {};
   for(var data in multiData) {
     var newDiv = document.createElement("div");
-    newDiv.innerHTML = 
-    for(var i = 0; i < 7; i++) {
-      multiData.data.week[0].probability;
-    }
+    newDiv.innerHTML = "<h3>${newDiv}</h3>";
+    newDiv.class = "game";
+    list.insertBefore(newDiv, list.firstChild);
   }
+  console.log("nwg");
 }
 
-function drawGraph(data) {
-  var myChart = new Chart(graph, {
-    type: "bar",
-    data: {
-      labels: labels;
-      datasets: [{
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)"
-        ],
-        borderColor: [
-          "rgba(255,99,132,1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)"
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      responsive: false;
-    }
-  });
-}
+// function drawGraph(data) {
+//   var myChart = new Chart(graph, {
+//     type: "bar",
+//     data: {
+//       labels: labels,
+//       datasets: [{
+//         data: [12, 19, 3, 5, 2, 3],
+//         backgroundColor: [
+//           "rgba(255, 99, 132, 0.2)",
+//           "rgba(54, 162, 235, 0.2)",
+//           "rgba(255, 206, 86, 0.2)",
+//           "rgba(75, 192, 192, 0.2)",
+//           "rgba(153, 102, 255, 0.2)",
+//           "rgba(255, 159, 64, 0.2)"
+//         ],
+//         borderColor: [
+//           "rgba(255,99,132,1)",
+//           "rgba(54, 162, 235, 1)",
+//           "rgba(255, 206, 86, 1)",
+//           "rgba(75, 192, 192, 1)",
+//           "rgba(153, 102, 255, 1)",
+//           "rgba(255, 159, 64, 1)"
+//         ],
+//         borderWidth: 1
+//       }]
+//     },
+//     options: {
+//       responsive: false;
+//     }
+//   });
+// }
--- routes/home.js
+++ routes/home.js
@@ -4,49 +4,22 @@ var connection = require("../other/connectMysql.js");
 var app = require("../app.js");
 
 var io;
+var data;
 
 router.get("/", function(req, res) {
   if(!io) {
     io = require("../bin/www");
     startSocket();
   }
-  res.render("home", {login: true});
+  res.render("home", {login: true, data: {"アイドルマスターシンデレラガールズ": true}});
 });
 
-router.post("/", function(req, res) {
-  if(req.body.gameName) {
-    connection.query("SELECT `gacha` FROM `users` WHERE `id` = ?",
-    [req.session.userId], function(err, result) {
-      if(err) {
-        console.error(err);
-        res.redirect("home", {error: "データベースエラー", login: true});
-        return;
-      }
-      var userData, provisionalData = result[0];
 
-      if(result[0][req.body.gameName]) {
-        res.render("home", {error: "既に存在しています", login: true, data: userData});
-        return;
-      }
-
-      userData[req.body.gameName] = {};
-
-      connection.query("UPDATE `users` FROM `gacha` = ?"
-      [userData], function(err, result) {
-        if(err) {
-          console.error(err);
-          res.redirect("home", {error: "データベースエラー", login: true, data: provisionalData});
-          return;
-        }
-        res.render("home", {login: true, data: userData});
-      });
-    });
-  }
-});
 
 function startSocket() {
-  io.sockets.on('connection', function(socket) {
-    socket.on('error',function(err) {
+  io.sockets.on("connection", function(socket) {
+    socket.emit("firstData", {data: data});
+    socket.on("error",function(err) {
       console.error(err)
     });
   });
