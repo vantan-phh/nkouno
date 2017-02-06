4d2735712a8979332f00d7275d0ce3d5e7e5adee
高槻やよい <qazwsxedcrfvtgbyhnujmikol@takatsukiyayoi.local>
2017-02-07 00:42:49 +0900

    earg

--- other/connectMysql.js
+++ other/connectMysql.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql2");
 
 var connection = mysql.createPool({
+  connectionLimit : 10,
   host : process.env.MYSQL_NKOU_HOST,
   user : process.env.MYSQL_NKOU_USER,
   password : process.env.MYSQL_NKOU_PASSWORD,
