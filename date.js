72b277dc15f73839f3f2d9568bb48e8d1579de22
高槻やよい <qazwsxedcrfvtgbyhnujmikol@takatsukiyayoi.local>
2017-02-07 00:08:50 +0900

    areg

--- date.js
+++ date.js
@@ -1,9 +1,10 @@
 var mysql = require("mysql2");
 
 var connection = mysql.createConnection({
-  host : "localhost",
-  user : "root",
-  database : "nkou"
+  host : process.env.MYSQL_NKOU_HOST,
+  user : process.env.MYSQL_NKOU_USER,
+  password : process.env.MYSQL_NKOU_PASSWORD, 
+  database : process.env.MYSQL_DATABACE
 });
 
 var date = new Date();
