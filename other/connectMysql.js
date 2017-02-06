721065b0f8812707d4fb274affb22e5a2ef06d73
高槻やよい <qazwsxedcrfvtgbyhnujmikol@takatsukiyayoi.local>
2017-02-07 00:26:38 +0900

    ergb

--- other/connectMysql.js
+++ other/connectMysql.js
@@ -4,7 +4,9 @@ var connection = mysql.createConnection({
   host : process.env.MYSQL_NKOU_HOST,
   user : process.env.MYSQL_NKOU_USER,
   password : process.env.MYSQL_NKOU_PASSWORD,
-  database : process.env.MYSQL_DATABACE
+  database : process.env.MYSQL_NKOU_DATABACE
 });
+
+connection.connect();
 
 module.exports = connection;
